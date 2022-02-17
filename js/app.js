88f9ebe940d343affc6387e26b56483f8ac8d96b
mahamudul5 <mh.bilash22@gmail.com>
2022-02-17 10:30:19 +0600

    js added

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// get input from income field and sent message for error.
+document.getElementById('Calculate-btn').addEventListener('click', function () {
+    // const myIncome = document.getElementById('income-input');
+    console.log("clicked");
+});
+
+

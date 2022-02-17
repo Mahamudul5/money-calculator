cc43b2d0022b09edea98822a785294b64615405d
mahamudul5 <mh.bilash22@gmail.com>
2022-02-17 17:58:01 +0600

    beta version updated

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 // get input from income field and sent message for error.
-// document.getElementById('Calculate-btn').addEventListener('click', function inputValidation() {
-// //     const myIncome = document.getElementById('income-input');
-// //     let totaIncome = parseFloat(myIncome.value);
 
-// });
+document.getElementById('Calculate-btn').addEventListener('click', function inputIncome() {
+    const myIncome = document.getElementById('income-input');
+    let totaIncome = parseFloat(myIncome.value);
+
+
+});
+
 document.getElementById('Calculate-btn').addEventListener('click', function inputValidation() {
     const myfood = document.getElementById('food');
     let totalFoodExpense = myfood.value;
@@ -23,6 +26,35 @@ document.getElementById('Calculate-btn').addEventListener('click', function inpu
     expencesFeild.innerText = total;
     let balanceField = document.getElementById('left-balance');
     balanceField.innerText = left;
+
+
+
+});
+document.getElementById('save-btn').addEventListener('click', function () {
+    const savingInput = document.getElementById('saving-input');
+    const myIncome = document.getElementById('income-input');
+    let totaIncome = parseFloat(myIncome.value);
+    let savingParsentage = parseInt(savingInput.value);
+
+    let finalSaving = (totaIncome * savingParsentage) / 100;
+    console.log(finalSaving);
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = finalSaving;
+    const remainingAmountField = document.getElementById('remaining-last-balance');
+    let forFood = document.getElementById('food');
+    let expenseForFood = parseInt(forFood.value);
+    let forRent = document.getElementById('rent');
+    let expenseForRent = parseInt(forRent.value);
+    let forCloths = document.getElementById('cloths');
+    let expenseForCloths = parseInt(forCloths.value);
+    let inTotalExpense = expenseForFood + expenseForRent + expenseForCloths;
+    console.log(inTotalExpense);
+    console.log(totaIncome);
+    console.log(finalSaving);
+
+    const finalSavingWithIntotalExpense = finalSaving + inTotalExpense;
+    const remainningAmount = totaIncome - finalSavingWithIntotalExpense;
+    remainingAmountField.innerText = remainningAmount;
 
 
 });

fd6bd08159e2baf6eab46cac246507e0a92f7f8c
mahamudul5 <mh.bilash22@gmail.com>
2022-02-17 22:53:39 +0600

    comment added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,31 @@
+// add buble event to easy to access all 
 document.getElementById('main-div').addEventListener('click', function (event) {
 
-
+    // call to Calculate button .
     document.getElementById('Calculate-btn').addEventListener('click', function () {
-
-
         const mainBalance = document.getElementById('income-input');
         const myIncome = parseInt(mainBalance.value);
-        // Error handling start
+        // Error handling start.
         if (isNaN(myIncome) || myIncome <= 0 || myIncome === '') {
-            alert('Please enter a valid number');
+            alert('Please enter a valid number thats you Earn.');
         };
 
         const food = document.getElementById('food');
         const myFood = parseInt(food.value);
+        // Error handling start.
         if (isNaN(myFood) || myFood <= 0 || myFood === '') {
             alert('Please enter a valid number in *Food');
         };
 
         const rent = document.getElementById('rent');
         const myRent = parseInt(rent.value);
+        // Error handling start.
         if (isNaN(myRent) || myRent <= 0 || myRent === '') {
             alert('Please enter a valid number in *Rent');
         };
         const cloths = document.getElementById('cloths');
         const myCloths = parseInt(cloths.value);
+        // Error handling start.
         if (isNaN(myCloths) || myCloths <= 0 || myCloths === '') {
             alert('Please enter a valid number in *Cloths');
         };
@@ -31,15 +33,21 @@ document.getElementById('main-div').addEventListener('click', function (event) {
         const totalExpenseField = document.getElementById('total-expences')
         totalExpenseField.innerText = totalExpense;
         const balance = myIncome - totalExpense;
+        // Error handling start.
+        if (totalExpense > myIncome) {
+            alert('oh! you havent enough money to spend.');
+        };
         const balanceFeild = document.getElementById('left-balance');
         balanceFeild.innerText = balance;
         document.getElementById('save-btn').addEventListener('click', function () {
             const saveInputField = document.getElementById('saving-input');
             const saving = parseInt(saveInputField.value);
+            // Error handling start.
             if (isNaN(saving) || saving <= 0 || saving === '') {
                 alert('Please enter a valid number in *Saving');
             };
             const finalSaving = (myIncome * saving) / 100;
+            // Error handling start.
             if (finalSaving > balance) {
                 alert('ups!!! you dont have enough money to saving');
             };
@@ -49,6 +57,7 @@ document.getElementById('main-div').addEventListener('click', function (event) {
             const remainningAmount = myIncome - totalExpenseAndFinalSaving;
             const remainingBalanceField = document.getElementById('remaining-last-balance');
             remainingBalanceField.innerText = remainningAmount;
+            // add a function to clear all input FileSystemDirectoryEntry. 
 
             clearInputValue(food);
             clearInputValue(rent);

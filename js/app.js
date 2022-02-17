56dd321dec6cb9a7308b23785c1556cbf2d78237
mahamudul5 <mh.bilash22@gmail.com>
2022-02-17 12:46:29 +0600

    main part calculation done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 // get input from income field and sent message for error.
-document.getElementById('Calculate-btn').addEventListener('click', function () {
-    // const myIncome = document.getElementById('income-input');
-    console.log("clicked");
+// document.getElementById('Calculate-btn').addEventListener('click', function inputValidation() {
+// //     const myIncome = document.getElementById('income-input');
+// //     let totaIncome = parseFloat(myIncome.value);
+
+// });
+document.getElementById('Calculate-btn').addEventListener('click', function inputValidation() {
+    const myfood = document.getElementById('food');
+    let totalFoodExpense = myfood.value;
+    let x = parseInt(totalFoodExpense);
+    const myRent = document.getElementById('rent');
+    let totalRentExpense = myRent.value;
+    let y = parseInt(totalRentExpense);
+    const myCloths = document.getElementById('cloths');
+    let totalClothsExpense = myCloths.value;
+    let z = parseInt(totalClothsExpense);
+    const myIncome = document.getElementById('income-input');
+    let totaIncome = parseFloat(myIncome.value);
+    let total = x + y + z;
+    let left = totaIncome - total;
+    console.log(left);
+    let expencesFeild = document.getElementById('total-expences');
+    expencesFeild.innerText = total;
+    let balanceField = document.getElementById('left-balance');
+    balanceField.innerText = left;
+
+
 });
 
 
